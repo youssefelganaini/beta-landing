dbd080b43263b5468b69551804103ff3733921f8
youssefelganaini <youssef.elganaini@tum.de>
2024-10-06 22:08:17 +0200

    trying tab icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head"; // Import Head
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="light">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en" className="light">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/public/favicon-32x32.png"
+          />
+        </Head>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   );
 }
